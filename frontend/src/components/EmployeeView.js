8e8090df214224b9e3c34c48da9cbb6a83f826e1
Justin Wu <justinwu820@gmail.com>
2021-09-28 19:53:49 -0400

    Removed warnings & Added styles

--- frontend/src/components/EmployeeView.js
+++ frontend/src/components/EmployeeView.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{ useEffect } from 'react'
+import Card from './Card'
 
 export default function EmployeeView() {
+    useEffect(() => {
+        // Fetch data from the backend
+
+    },[])
     return (
-        <div>
-            Review all the employees
-        </div>
+       <>
+            <h1 className="text-center">Employees List</h1>
+            <div className="mt-5 row row-cols-1 row-cols-md-4 g-5">
+            </div>
+       </>
     )
 }
--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -6,7 +6,7 @@ export default function SearchBar() {
         <h1 className="mt-5 form-label text-center">Search Employees by name</h1>
         <div className="d-flex justify-content-center">
             <input style={{width:"70%"}}type="text" id="inputPassword5" className="form-control"/>
-            <button style={{verticalAlign:'top'}}type="button" class="btn btn-primary">Search </button>
+            <button style={{verticalAlign:'top'}}type="button" className="btn btn-primary">Search </button>
         </div>
     </>
     )
