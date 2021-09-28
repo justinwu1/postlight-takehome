dfc4c2b6fee36fa8f751e463892a28dd7686cc9e
Justin Wu <justinwu820@gmail.com>
2021-09-28 17:10:35 -0400

    Added a few navigation

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom'
 export default function NavBar() {
     return (
     <div>
-            <nav class="navbar navbar-expand-lg navbar-light ">
-                <div class="container-fluid">
-                    <Link class="navbar-brand" to="/">Postlight</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Postlight</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <Link class="nav-link active" aria-current="page" to="/employees">View All Employee</Link>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" to="/employees">View All Employee</Link>
                     </li>
             </ul>
     </div>
