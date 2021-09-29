306969c99709011441170f5a2601c9ea5c1f1f8b
Justin Wu <justinwu820@gmail.com>
2021-09-28 20:42:35 -0400

    Created reusable Card component and able to display all employee data

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-export default function Card() {
+export default function Card(props) {
+    const { lastName,firstName,zipcode,streetAddress,country,state,city,portfolioImg,jobTitle,jobType,department } = props.data;
+    console.log(props.data)
     return (
         <div className="col">
             <div className="card">
-                <img src="http://placeimg.com/640/480/people" className="card-img-top" alt="..." />
+                <img src={portfolioImg} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 className="card-title">{`${firstName} ${lastName}`}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{jobTitle}</h6>
                 </div>
                 <ul className="list-group list-group-flush">
-                    <li className="list-group-item">An item</li>
-                    <li className="list-group-item">A second item</li>
-                    <li className="list-group-item">A third item</li>
+                    <li className="list-group-item"><b>Department:</b> {`${department}`}</li>
+                    <li className="list-group-item"><b>Job type:</b> {`${jobType}`}</li>
+                    <li className="list-group-item"><b>Address:</b> {`${streetAddress},${city},${state},${zipcode}`}</li>
+                    <li className="list-group-item"><b>Born:</b> {`${country}`}</li>
                 </ul>
                 <div className="card-body">
                     <Link to="#" className="card-link">Card link</Link>
